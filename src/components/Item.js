334fe987d64452f88fcab68c2e6b27e9713bf287
meshackmesto <meshackorina93@gmail.com>
2024-05-03 18:59:48 +0300

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Item({ name, category }) {
+  const [isCart, setIsCart] = useState(false);
+
+
+  function handleCart(){
+    setIsCart((isCart) =>!isCart);
+  }
+const color = isCart ? 'purple' : 'yellow';
+const onCart = isCart ? 'in-Cart' : "";
+
   return (
-    <li className="">
+    <li className={onCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+      className={isCart ? "Remove" : "Add"}
+      onClick={handleCart} style={{background : color}}>
+        {isCart ? "Remove From" : "Add To"}Cart
+        </button>
     </li>
   );
 }
